aebdc6455884b087f10c8a706015804f7c80abe6
Fausto Junqueira <faustoferjunqueira@gmail.com>
2022-08-08 14:48:43 -0300

    atualizado o bdd

---
+++ SpMercantil/Core/Exceptions/RecordAlreadyStoredException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Core.Exceptions
+{
+    public class RecordNotFoundException : Exception
+    {
+        public string Id { get; }
+        public string Type { get; }
+
+        public RecordNotFoundException(string id, string type, Exception inner) : base($"Record id={id} not found. Type={type}", inner)
+        {
+            Id = id;
+            Type = type;
+        }
+
+        public RecordNotFoundException(string id, string type) : this(id, type, null) { }
+    }
+}
